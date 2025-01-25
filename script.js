769f15c31d5f30055e1cd1826d2749abc4fd699e
Pravallika-Desu <pravallikadesu34@gmail.com>
2025-01-25 12:10:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for your message!');
+});
+document.getElementById('project1').addEventListener('click', function () {
+    window.location.href = 'p1.html'; // Navigate to p1.html
+});
+document.getElementById('project2').addEventListener('click', function () {
+    window.location.href = 'p2.html'; // Navigate to p1.html
+});
+document.getElementById('more').addEventListener('click', function () {
+    window.location.href = 'more.html'; // Navigate to p1.html
+});
